f5928f5ebdec60f3d9979fa65bc1b53424e8c11f
amueedk <abdulmueedkhan23@gmail.com>
2025-04-30 21:36:27 +0500

    Updated tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 from app import app, db, User, Complaint
 from flask_login import login_user
